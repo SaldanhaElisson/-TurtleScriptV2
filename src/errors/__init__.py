a077e4136457208dcd34800c7301c0be8b03e5e9
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2025-07-01 17:06:01 -0300

    feat: lexical analyzer.

---
+++ src/errors/__init__.py
@@ -0,0 +1,3 @@
+from .error import GenericError, LexicalError, SemanticError, SyntacticError
+from .error_messages import ErrorMessages
+from .error_types import ErrorType
---
+++ src/main.py
@@ -0,0 +1,31 @@
+import sys
+from pprint import pprint
+
+from lexical_analyzer.tokenizer import Tokenizer
+from lexical_analyzer.utils.lexer import Lexer
+from lexical_analyzer.utils.symbol_table import SymbolTable
+from lexical_analyzer.utils.token_factory import TokenTypeFactory
+from lexical_analyzer.utils.token_list import TokenListTable
+
+
+def main(args):
+    if not args:
+        raise ValueError("no input file")
+    path = args[0]
+    symbol_table = SymbolTable()
+    token_type_factory = TokenTypeFactory(symbol_table)
+    token_list_table = TokenListTable(token_type_factory)
+    lexer = Lexer()
+    tokenizer_obj = Tokenizer(lexer, token_list_table, symbol_table, token_type_factory)
+    tokenizer_obj.analise_line(path)
+
+    print("List<TokenList>: \n")
+    pprint(token_list_table.get_tokens())
+    print("\n")
+    print("List<SymbolsTable>: \n")
+    pprint(symbol_table.get_symbols())
+    print("\n")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
