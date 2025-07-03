14e303b83346412b2bd47c4dedceecdf02e81907
joselisboaa <joselisboaa05@gmail.com>
2025-07-03 10:08:51 -0300

    feat: apply compiladores 2025.1 example

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from src.code_generator.generator import CodeGenerator
-from tests.test_semantic_analyzer import AST_Input_1, AST_Input_2, AST_Input_3 
+from tests.test_semantic_analyzer import AST_Input_1, AST_Input_2, AST_Input_3, AST_Input_4 
 
 generator = CodeGenerator()
 
@@ -13,4 +13,8 @@ print(python_code)
 
 print("\nGERAÇÃO DO CÓDIGO 3: \n")
 python_code = generator.generate(AST_Input_3)
+print(python_code)
+
+print("\n GERAÇÃO DO CÓDIGO 4: \n")
+python_code = generator.generate(AST_Input_4)
 print(python_code)
