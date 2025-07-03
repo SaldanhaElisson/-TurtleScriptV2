8cfa4a59c685be0677a765e8c429973ab7f55ea8
joselisboaa <joselisboaa05@gmail.com>
2025-07-03 09:50:23 -0300

    feat: create new command and put comments in code

--- tests/test_semantic_analyzer.py
+++ tests/test_semantic_analyzer.py
@@ -5,6 +5,7 @@ from src.semantic_analyzer.analyzer import analyze_program
 AST_Input_1 = Program(
     declarations=[],
     commands=[
+        Comment("Desenha as quatro arestas do quadrado"),
         Command("avancar", Literal(150, "inteiro")),
         Command("girar_direita", Literal(90, "inteiro")),
         Command("avancar", Literal(150, "inteiro")),
@@ -21,6 +22,7 @@ AST_Input_2 = Program(
     declarations=[VariableDeclaration("inteiro", ["tamanho_lado"])],
     commands=[
         Assignment("tamanho_lado", Literal(200, "inteiro")),
+        Comment("Desenha uma estrela de 5 pontas"),
         Command("avancar", VariableReference("tamanho_lado")),
         Command("girar_direita", Literal(144, "inteiro")),
         Command("avancar", VariableReference("tamanho_lado")),
@@ -49,6 +51,7 @@ AST_Input_3 = Program(
             body=[
                 Comment("Muda a cor da linha a cada iteração"),
                 Command("definir_cor", Literal("cyan", "texto")),
+                Comment("Desenha e aumenta o lado"),
                 Command("avancar", VariableReference("lado")),
                 Command("girar_direita", Literal(90, "inteiro")),
                 Assignment("lado",
@@ -60,7 +63,8 @@ AST_Input_3 = Program(
                 ),
             ]
         ),
-        Command("escrever", Literal("Teste", "texto"))
+        Command("escrever", Literal("Teste", "texto")),
+        Command("posicao_atual", []),
     ]
 )
 
