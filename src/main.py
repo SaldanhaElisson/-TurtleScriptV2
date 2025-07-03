260c528dbfb72bdc39d236c3a2700e11074536b3
joselisboaa <joselisboaa05@gmail.com>
2025-07-02 21:55:05 -0300

    feat: instancing the generator in each example

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from src.code_generator.generator import CodeGenerator
+from tests.test_semantic_analyzer import AST_Input_1, AST_Input_2, AST_Input_3 
+
+generator = CodeGenerator()
+
+print("\nGERAÇÃO DO CÓDIGO 1: \n")
+python_code = generator.generate(AST_Input_1)
+print(python_code)
+
+print("\nGERAÇÃO DO CÓDIGO 2: \n")
+python_code = generator.generate(AST_Input_2)
+print(python_code)
+
+print("\nGERAÇÃO DO CÓDIGO 3: \n")
+python_code = generator.generate(AST_Input_3)
+print(python_code)
